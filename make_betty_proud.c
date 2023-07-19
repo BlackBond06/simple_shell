b464190e2869f6ee405970a7e6c58d1b33595c1d
BlackBond06 <eleazuosita007@yahoo.com>
2023-07-19 03:10:37 -0700

    Prints welcome message

---
+++ make_betty_proud.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints welcome to the simple shell
+ * project, on the terminal.
+ *
+ * Return: Always 0 on success.
+ *
+ */
+
+int main(void)
+{
+
+	printf("Welcome to the simple shell  project!\n");
+
+	return (0);
+}
